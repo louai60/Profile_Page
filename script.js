b282405f67040bdcaee323e73486ae9d0988b64d
louai <louaysaafi60@gmail.com>
2024-02-07 13:51:50 +0100

    Making Connections

---
+++ script.js
@@ -0,0 +1,19 @@
+var element1 = document.getElementById("conn-1");
+var element2 = document.getElementById("conn-2");
+
+function remove() {
+    element1.remove("conn");
+}
+
+function remove2() {
+    element2.remove("conn");
+}
+
+//changing Name 
+
+const text = document.querySelector('.name');
+
+function changeName(){
+    text.innerText = "Louay";
+}
+
